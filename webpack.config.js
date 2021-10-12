622aa70287ad7d809eef909de91413b5d38a4ab9
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-12 16:48:30 -0300

    Se agregan plugins y loaders para react

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      //inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+    watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true, //Hace que se abra en el navegador
+  },
+};
